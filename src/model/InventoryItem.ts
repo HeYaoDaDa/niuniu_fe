08b7d7d90ae684bbc7f41985d26af0eac07b2951
HeYaoDaDa <3050166231@qq.com>
2025-02-27 23:55:17 +0800

    remove inventory

--- src/model/InventoryItem.ts
+++
@@ -1,7 +0,0 @@
-import type { ShallowRef } from "vue";
-import type { Item } from "./data/Item";
-
-export interface InventoryItem {
-    item: ShallowRef<Item>;
-    amount: number;
-}
--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -1,15 +1,14 @@
-import type { InventoryItem } from '@/model/InventoryItem';
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue';
+import { computed, reactive, shallowReactive, type ShallowReactive } from 'vue';
 import type { Item } from '@/model/data/Item';
 import { useGameDataStore } from './gameData';
 
 export const useInventoryStore = defineStore('inventory', () => {
   const gameDataStore = useGameDataStore();
 
-  const inventoryItemMap = ref(new Map<string, InventoryItem>());
+  const inventoryItemMap = reactive(new Map<string, InventoryItem>());
 
-  const inventoryItems = computed(() => Array.from(inventoryItemMap.value.values()).sort((a, b) => a.item.sort - b.item.sort));
+  const inventoryItems = computed(() => Array.from(inventoryItemMap.values()).sort((a, b) => a.item.sort - b.item.sort));
 
   function add(itemOrId: Item | string, amount: number) {
     let item;
@@ -22,15 +21,15 @@ export const useInventoryStore = defineStore('inventory', () => {
     } else {
       item = itemOrId
     }
-    const existItem = inventoryItemMap.value.get(item.id);
+    const existItem = inventoryItemMap.get(item.id);
     if (existItem) {
       existItem.amount += amount;
     } else {
-      inventoryItemMap.value.set(item.id, { item, amount });
+      inventoryItemMap.set(item.id, new InventoryItem(item, amount));
     }
   }
   function remove(id: string, amount: number) {
-    const existItem = inventoryItemMap.value.get(id);
+    const existItem = inventoryItemMap.get(id);
     if (existItem) {
       if (existItem.amount > amount) {
         existItem.amount -= amount;
@@ -38,7 +37,7 @@ export const useInventoryStore = defineStore('inventory', () => {
         if (existItem.amount < amount) {
           console.error(`${id} amount ${existItem.amount} < ${amount}`);
         }
-        inventoryItemMap.value.delete(id);
+        inventoryItemMap.delete(id);
       }
     } else {
       console.error(`Not exist inventory item id ${id}`);
@@ -51,3 +50,13 @@ export const useInventoryStore = defineStore('inventory', () => {
     remove
   }
 })
+
+class InventoryItem {
+  item: ShallowReactive<Item>;
+  amount: number;
+
+  constructor(item: Item, amount: number) {
+    this.item = shallowReactive(item);
+    this.amount = amount;
+  }
+}
